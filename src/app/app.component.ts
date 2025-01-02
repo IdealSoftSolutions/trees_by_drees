1200138362ce0babca818b58d2e8eaf8611b66d7
IdealSoftSolutions <solutionsidealsoft@gmail.com>
2025-01-01 18:13:00 -0600

    header changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router'; // Import RouterModule
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, FooterComponent, RouterModule], // Include RouterModule
+  imports: [HeaderComponent, FooterComponent, RouterModule, CommonModule], // Include RouterModule
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'], // Fix typo
   standalone: true, // If you're using standalone components
