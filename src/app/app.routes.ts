bb9c3263f16fb13f7492b2755fabb384f05d6672
IdealSoftSolutions <solutionsidealsoft@gmail.com>
2025-01-03 14:39:54 -0600

    added getquote form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { EnrollmentFormComponent } from './enrollment-form/enrollment-form.component';
+import { GetQuoteFormComponent } from './get-quote/get-quote.component';
 
 export const routes: Routes = [
-    {path:'',component:HomeComponent},
-    {path:'enroll',component:EnrollmentFormComponent}
+    { path: '', component: HomeComponent },
+    { path: 'enroll', component: EnrollmentFormComponent },
+    { path: 'getQuote', component: GetQuoteFormComponent }
 ];
